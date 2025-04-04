ba73f471de7d7b5f750711158047c3136f79d27f
joao-rafael-paton <joaorafaelpaton@gmail.com>
2025-04-04 20:48:02 -0300

    Update pacienteDados.php
    Adicionando func para criar ID paciente

--- laboratorio/processamento/pacienteDados.php
+++ laboratorio/processamento/pacienteDados.php
@@ -3,34 +3,21 @@
 require_once '../classes/Paciente.php';
 require_once '../classes/Exame.php';
 
-/**
- * Calcula a idade com base na data de nascimento
- * @param string $dataNascimento Data no formato aceito pelo DateTime
- * @return int Idade em anos
- */
-function calcularIdade($dataNascimento) {
-    $nascimento = new DateTime($dataNascimento);
-    $hoje = new DateTime();
-    return $hoje->diff($nascimento)->y;
-}
 // Pegando os dados do formulário
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $idPaciente = $_POST['registro'];
+    $idPaciente = gerar_id_aleatorio(8);
     $dataCadastro = $_POST['dataCadastro'];
     $nomeCompleto = $_POST['nome-completo'];
     $dataNascimento = $_POST['dataNascimento'];
     $telefone = $_POST['telefone'];
     $email = $_POST['email'];
     $nomeMae = $_POST['nome-mae'];
-    // Calcula a idade do paciente
     $idadePaciente = calcularIdade($dataNascimento);
     $nomeMedicamento = $_POST['remedio-nome'];
     $nomePatologia = $_POST['patologia_nome'];
     $laboratorio = $_POST['laboratorio'];
     $exameTexto = $_POST['exameTexto'];
-    
 
-    
     // Verifica se o paciente tem medicamento
     $nomeMedicamento = ($_POST['remedio'] == 'medicamento-sim') 
         ? $_POST['nome_medicamento'] 
@@ -60,6 +47,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $laboratorio, 
         $exameTexto
     );
+}
+
+/**
+ * Calcula a idade com base na data de nascimento
+ * @param string $dataNascimento Data no formato aceito pelo DateTime
+ * @return int Idade em anos
+ */
+function calcularIdade($dataNascimento) {
+    $nascimento = new DateTime($dataNascimento);
+    $hoje = new DateTime();
+    return $hoje->diff($nascimento)->y;
+}
+
+/*
+ *  Gera id aleatorio para paciente de 8 caracteres somente com números 
+ *  e letras maiúsculas
+ */
+function gerar_id_aleatorio($tamanho = 8) {
+    $caracteres = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $idPaciente = '';
     
+    for ($i = 0; $i < $tamanho; $i++) {
+        $idPaciente .= $caracteres[random_int(0, strlen($caracteres) - 1)];
+    }
+    
+    return $idPaciente;
 }
 ?>
